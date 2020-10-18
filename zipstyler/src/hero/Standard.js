b356f82e73a27de4ea9fe4e77f0b9c15a1af26e7
carlsachs <sachscarl@gmail.com>
2020-10-17 22:30:00 -0400

    fixed eslint error with tailwind

---
+++ zipstyler/src/hero/Standard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import tw from "twin.macro";
+import styled from "styled-components";
+import { css } from "styled-components/macro";
+
+const Wrapper = tw.div`container bg-blue-500 height:100`;
+
+export default () => {
+
+   return (
+       <Wrapper>
+       
+       </Wrapper>
+   ) 
+    
+}
